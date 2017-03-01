640d8023da28731813682de6cae5dd0b5d47bb45
jsap1904 <sapinoso.j@gmail.com>
2017-02-28 19:50:35 -0800

    add

---
+++ js/main.js
@@ -0,0 +1,41 @@
+//shows up on console
+//console.log("herro world")
+//shows on web page
+//document.write("herro world on doc")
+
+//var username = prompt("what is your name?")
+
+// //console.log(username)
+// //numbers
+// var num = 1234
+// //true or false
+// var boolean = true
+// //string of text
+// var string = "string"
+// //list of variable types
+// var array = [boolean, 1234, "string", true]
+
+// var string2 = "nick's least favorite"
+
+// var comboBreakerStringOfLife = string2 + string
+
+// if(2>1 && 2>3) {
+// 	//do this
+// 	document.write("yes 2 is greater than 1")
+// }
+
+// else {
+// 	//do this
+// 	document.write("nope")
+// }
+
+// //for (i = 0; i <= 20; i = i + 1) {
+// 	//do this
+// 	console.log(i)
+// }
+
+var username = prompt("what is your name?")
+
+while(username != "matt"){
+	username = prompt('what is your name?')
+}
